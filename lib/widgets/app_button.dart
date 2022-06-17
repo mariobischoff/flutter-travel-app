5c324251b76f7523c15fe171b4d5f7021fe1ed7a
Mario Bischoff <mariobischoffneto@hotmail.com>
2022-06-17 15:35:55 -0300

    first commit

---
+++ lib/widgets/app_button.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:travel_app/widgets/app_text.dart';
+
+import '../misc/colors.dart';
+
+class AppButton extends StatelessWidget {
+  final Color color;
+  final Color backgroundColor;
+  String? text;
+  IconData? icon;
+  double size;
+  final Color borderColor;
+  bool? isIcon;
+
+  AppButton(
+      {Key? key,
+      this.isIcon = false,
+      this.text = 'Hi',
+      this.icon,
+      required this.size,
+      required this.color,
+      required this.backgroundColor,
+      required this.borderColor})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: size,
+      height: size,
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: borderColor,
+          width: 1.0,
+        ),
+        borderRadius: BorderRadius.circular(15),
+        color: backgroundColor,
+      ),
+      child: isIcon == false
+          ? Center(
+              child: AppText(
+                text: text!,
+                color: color,
+              ),
+            )
+          : Center(
+              child: Icon(
+                icon,
+                color: color,
+              ),
+            ),
+    );
+  }
+}
